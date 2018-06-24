b9c6cad0bf5b6ef0526f5c7dd41fa122c0417bd3
Christian Stigen Larsen <csl@csl.name>
2018-06-25 00:57:10 +0200

    Just trying something

--- gazerbeam/viewer.py
+++ gazerbeam/viewer.py
@@ -32,34 +32,54 @@ class Canvas(object):
                 symbol='o',
                 parent=self.view.scene)
 
-        self.labels = vispy.scene.Text(
-                pos=self.pos - [0, 0.0025, 0],
-                color=vispy.color.Color([1,1,1]),
-                anchor_y="top",
-                text="Howdy",
-                parent=self.view.scene)
+        self.labels = [
+                vispy.scene.Text(
+                    pos=self.pos[0] - [0, 0.0025, 0],
+                    color=vispy.color.Color([1,1,1]),
+                    anchor_y="top",
+                    text="<root>",
+                    parent=self.view.scene)
+                ]
 
         self.view.camera.set_range()
 
-        self.received = []
         self.timer = vispy.app.Timer(interval="auto",
                                      connect=self._poll_queue, start=True)
 
     def _poll_queue(self, event=None):
         obj = gazerbeam.ipc.Queue.receive()
 
-        if obj is not None:
-            name = multiprocessing.current_process().name
-            self.received.append(obj)
-            print("%s: Received object %s (%d objs)" % (name, obj,
-                len(self.received)))
+        if obj is None:
+            return
 
-            if obj == "STOP":
-                if hasattr(self, "timer"):
-                    self.timer.stop()
-                gazerbeam.ipc.Queue.queue.close()
-                gazerbeam.ipc.Queue.queue.join_thread()
-                vispy.app.quit()
+        if obj == "STOP":
+            if hasattr(self, "timer"):
+                self.timer.stop()
+            gazerbeam.ipc.Queue.queue.close()
+            gazerbeam.ipc.Queue.queue.join_thread()
+            vispy.app.quit()
+            return
+
+        if isinstance(obj, tuple):
+            self._decode(obj)
+
+    def _decode_obj(obj):
+        wallclock, cpuclock, cutpoint, frames, args, kw, retval = obj
+        self.pos = np.append(self.pos[-1] - [0, 0.003, 0])
+
+        self.markers = vispy.scene.Markers(
+                pos=self.pos,
+                symbol='o',
+                parent=self.view.scene)
+
+        self.labels = [
+                vispy.scene.Text(
+                    pos=self.pos[0] - [0, 0.0025, 0],
+                    color=vispy.color.Color([1,1,1]),
+                    anchor_y="top",
+                    text="<root>",
+                    parent=self.view.scene)
+                ]
 
 
 def start():
